ce4a5afaf9edaef8c1536c78193d47d2e56a6e69
julivasquez567-boop <julivasquez567@gmail.com>
2026-06-03 16:39:53 -0600

    f

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+// BOTÓN VOLVER ARRIBA
+
+const btnArriba = document.getElementById("arriba");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        btnArriba.style.display = "block";
+    } else {
+        btnArriba.style.display = "none";
+    }
+});
+
+btnArriba.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
