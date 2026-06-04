146b270a95b98bd8f4d0cd285ad4980d4cf92b34
julivasquez567-boop <julivasquez567@gmail.com>
2026-06-03 19:11:06 -0600

    listo

--- script.js
+++ script.js
@@ -24,17 +24,21 @@ if(btnArriba){
 
 const btnArriba = document.getElementById("arriba");
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        btnArriba.style.display = "block";
-    } else {
-        btnArriba.style.display = "none";
-    }
-});
+if (btnArriba) {
 
-btnArriba.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            btnArriba.style.display = "flex";
+        } else {
+            btnArriba.style.display = "none";
+        }
     });
-});
+
+    btnArriba.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+}
