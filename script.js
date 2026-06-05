bac8e87638a14e1db15e7516db86b4038563e6df
julivasquez567-boop <julivasquez567@gmail.com>
2026-06-05 17:15:28 -0600

    s

--- script.js
+++ script.js
@@ -1,49 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-```
-// BOTÓN VOLVER ARRIBA
-const btnArriba = document.getElementById("arriba");
+    const modoBtn = document.getElementById("modoBtn");
 
-if (btnArriba) {
+    console.log("Botón encontrado:", modoBtn);
 
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 300) {
-            btnArriba.style.display = "flex";
-        } else {
-            btnArriba.style.display = "none";
-        }
-    });
+    if(modoBtn){
 
-    btnArriba.addEventListener("click", () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
+        modoBtn.addEventListener("click", () => {
+
+            console.log("Click detectado");
+
+            document.body.classList.toggle("dark");
+
         });
-    });
-}
 
-// MODO OSCURO
-const modoBtn = document.getElementById("modoBtn");
-
-if (modoBtn) {
-
-    if (localStorage.getItem("modo") === "oscuro") {
-        document.body.classList.add("dark");
-        modoBtn.textContent = "☀️";
     }
-
-    modoBtn.addEventListener("click", () => {
-        document.body.classList.toggle("dark");
-
-        if (document.body.classList.contains("dark")) {
-            localStorage.setItem("modo", "oscuro");
-            modoBtn.textContent = "☀️";
-        } else {
-            localStorage.setItem("modo", "claro");
-            modoBtn.textContent = "🌙";
-        }
-    });
-}
-```
 
 });
